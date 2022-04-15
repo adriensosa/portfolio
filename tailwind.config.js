4ef198f21e4a57979e8de2eb0ca01de64543a8c6
Adrien Sosa <hello@adriensosa.fr>
2022-04-16 00:56:29 +0200

    tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+      grey: {
+        light: "#cffafe",
+        regular: "#EAEDE4",
+        dark: "#00c4f3",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
