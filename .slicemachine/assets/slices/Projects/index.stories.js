30b2ab911419e18e97bfb64dff5d4659ccdcfb49
Adrien Sosa <hello@adriensosa.fr>
2022-04-21 23:20:15 +0200

    prismic integration

---
+++ .slicemachine/assets/slices/Projects/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Projects';
+
+export default {
+  title: 'slices/Projects'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"projects","items":[],"primary":{"title":[{"type":"heading1","text":"Exploit bricks-and-clicks e-business","spans":[]}],"description":[{"type":"paragraph","text":"Dolore sit labore ullamco pariatur cillum do do labore cillum aliqua reprehenderit pariatur dolore ipsum.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
